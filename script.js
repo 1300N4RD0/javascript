d7c381adb4f2d92910db499d5f72cefd07e5dc57
1300N4RD0 <144732232+1300N4RD0@users.noreply.github.com>
2024-01-18 14:01:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Your JavaScript code goes here
+ console.log("Hello, World!");
+
+console.log(myVariable);
+let myVariable = "Hello, JS!";
+alert("JS is fun!");
+
+
+console.log(MiVariable);
+let MiVariable = "300 + 5,000= 5,300";
